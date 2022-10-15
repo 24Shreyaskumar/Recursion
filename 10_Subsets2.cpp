53c088f3375b59722752542442e70884e6f06814
24Shreyaskumar <shreyaskumar.12010414@lpu.in>
2022-10-15 22:36:32 +0530

    commit on master

---
+++ 10_Subsets2.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define F(i, n, m) for (int i = n; i < m; i++)
+using namespace std;
+
+void display(vvi sol) {
+    cout<<"[";
+    for (auto i : sol) {
+        cout<<"[ ";
+        for (auto j : i) cout<<j<<" ";
+        cout<<"]";
+    }
+    cout<<"]\n";
+    return;
+}
+
+void Solution(vi v, vi &ans, vvi &sol, int n, int it) {
+    sol.push_back(ans);
+    F(i, it, n) {
+        if (i != it && v[i] == v[i-1]) continue;
+        ans.push_back(v[i]);
+        Solution(v, ans, sol, n, i+1);
+        ans.pop_back();
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin>>n;
+    vi v, ans;
+    vvi sol;
+    F(i, 0, n) {
+        int x; cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+
+    Solution(v, ans, sol, n, 0);
+    display(sol);
+    return 0;
+}
