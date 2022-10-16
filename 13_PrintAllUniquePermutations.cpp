e458e8c572ede2ebee9bfaf7d34da7b7e5b0b6e5
24Shreyaskumar <shreyaskumar.12010414@lpu.in>
2022-10-16 17:53:45 +0530

    commit on master

---
+++ 13_PrintAllUniquePermutations.cpp
@@ -0,0 +1,52 @@
+//time complexity = 
+//space complexity = 
+//auxillary space complexity = O(n)
+
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define vvi vector<vector<int>> 
+#define F(i, n, m) for (int i = n; i < m; i++)
+using namespace std;
+
+void display(vvi ans) {
+    cout<<"\nOutput : \n";
+    for (auto i : ans) {
+        for (auto j : i) cout<<j<<" ";
+        cout<<"\n";
+    }
+    return;
+}
+
+void Solution(vi &v, vvi &ans, int it) {
+    if (it == v.size()) {
+        ans.push_back(v);
+        return;
+    }
+
+    F(i, it, v.size()) {
+        if (i > it && v[i] == v[i-1]) continue;
+        swap(v[i], v[it]);
+        Solution(v, ans, it+1);
+        swap(v[i], v[it]);
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin>>n;
+    vi v;
+    F(i, 0, n) {
+        int x; cin>>x;
+        v.push_back(x);
+    }
+    vvi ans;
+
+    Solution(v, ans, 0);
+    display(ans);
+    
+    return 0;
+}
---
+++ 14_NQueens.cpp
@@ -0,0 +1,66 @@
+//Problem of Bactracking
+//time complexity = 
+//space complexity =
+//auxillary space complexity = 
+
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define vs vector<string>
+#define vb vector<bool>
+#define vvs vector<vector<string>>
+#define F(i, n, m) for (int i = n; i < m; i++)
+using namespace std;
+
+void display(vvs ans) {
+    for (auto i : ans) {
+        for (auto j : i) {
+            F(k, 0, ans.size()) {
+                cout<<j[k]<<" ";
+            }
+            cout<<"\n";
+        }
+        cout<<"\n\n";
+    }
+
+    return;
+}
+
+void Solution(vs &board, vb &left, vb &upLeft, vb &downLeft, vvs &ans, int col) {
+    if (col == board.size()) {
+        ans.push_back(board);
+        return;
+    }
+
+    F(row, 0, board.size()) {
+        if (!left[row] && !upLeft[row + col] && !downLeft[board.size()-1 + col - row]) {
+            board[row][col] = 'Q';
+            left[row] = true;
+            upLeft[row + col] = true;
+            downLeft[board.size()-1 + col - row] = true;
+            Solution(board, left, upLeft, downLeft, ans, col+1);
+            downLeft[board.size()-1 + col - row] = false;
+            upLeft[row + col] = false;
+            left[row] = false;
+            board[row][col] = '.';
+        }
+    }
+
+    return;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin>>n;
+    string sq;
+    F(i, 0, n) sq += '.';
+    vs board(n, sq);
+    vb left(n, false), upLeft(2*n - 1, false), downLeft(2*n - 1, false);
+    vvs ans;
+
+    Solution(board, left, upLeft, downLeft, ans, 0);
+    display(ans);
+
+    return 0; 
+}
