2818dcd596c4631de283330795569b1075d59503
24Shreyaskumar <shreyaskumar.12010414@lpu.in>
2022-10-18 21:54:21 +0530

    commit on master

---
+++ 15_SudokuSolver.cpp
@@ -0,0 +1,36 @@
+//Backtracking
+//time complexity = 
+//space complexity = 
+//auxillary space complexity = 
+
+#include <bits/stdc++.h>
+#define vi vector<int>
+#define vvi vector<vector<int>> 
+#define vs vector<string>
+#define vb vector<bool>
+#define F(i, n, m) for (int i = n; i < m; i++)
+using namespace std;
+
+void display() {
+
+}
+
+void Solution() {
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin>>n;
+    string sq;
+    F(i, 0, n) sq += ".";
+
+
+    Solution();
+    display();
+
+    return 0;
+}
